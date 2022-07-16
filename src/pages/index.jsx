7a42696e8a108016d719a43b9c5bce0ede61fa6d
basitol <basitolaitan27@gmail.com>
2022-07-16 23:44:27 +0100

    build: export for pages

---
+++ src/pages/index.jsx
@@ -0,0 +1,8 @@
+export { default as Ecommerce } from "./Ecommerce";
+export { default as Orders } from "./Orders";
+export { default as Product } from "./Product";
+export { default as Employees } from "./Employees";
+export { default as Editor } from "./Editor";
+export { default as Customers } from "./Customers";
+export { default as ColorPicker } from "./ColorPicker";
+export { default as Calendar } from "./Calendar";
